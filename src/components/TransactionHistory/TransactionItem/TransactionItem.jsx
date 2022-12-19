91bcb66fec4b219a20cda9fe03d99405193db3ef
Ivan-GoIT <educationzac@gmail.com>
2022-12-19 22:58:07 +0200

    fix last

--- src/components/TransactionHistory/TransactionItem/TransactionItem.jsx
+++ src/components/TransactionHistory/TransactionItem/TransactionItem.jsx
@@ -1,8 +1,9 @@
+import css from './TransactionItem.module.css'
 export const TransactionItem = ({ type, amount, currency, rowColor }) => (
-  <tr className={rowColor}>
-    <td>{type}</td>
-    <td>{amount}</td>
-    <td>{currency}</td>
+  <tr>
+    <td className={css[rowColor]}>{type}</td>
+    <td className={css[rowColor]}>{amount}</td>
+    <td className={css[rowColor]}>{currency}</td>
   </tr>
 );
 
